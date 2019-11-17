b117691938ec1375fe9166200eef6a049bd72e5e
TomaszSiwiec <ts@onet.pl>
2019-11-17 14:05:57 +0100

    First commit

---
+++ src/main/java/com/pk/electionappserver/electionappserver/ElectionappserverApplication.java
@@ -0,0 +1,13 @@
+package com.pk.electionappserver.electionappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionappserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectionappserverApplication.class, args);
+    }
+
+}
