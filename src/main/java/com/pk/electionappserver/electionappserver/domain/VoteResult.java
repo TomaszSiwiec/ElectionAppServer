3b4bc0903d709e7cc439b9259881836a4c884837
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2019-11-24 16:10:57 +0100

    [TO1] Added VoteResult class

--- src/main/java/com/pk/electionappserver/electionappserver/domain/VoteResult.java
+++ src/main/java/com/pk/electionappserver/electionappserver/domain/VoteResult.java
@@ -2,9 +2,27 @@ package com.pk.electionappserver.electionappserver.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity(name = "VotesResults")
 public class VoteResult {
-    //TODO: ENCJA DO OMÓWIENIA
-    //@Column(name = "user")
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "user")
+    private User user;
+
+    @Column(name = "election")
+    private Election election;
+
+    @Column(name = "candidate")
+    private Candidate candidate;
+
+    @Column(name = "dataTime")
+    private LocalDateTime localDateTime;
 }
