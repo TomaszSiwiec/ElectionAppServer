ffbf1206a40a5c0388d5007b77206885599536fe
aezakmiDev <57116176+aezakmiDev@users.noreply.github.com>
2019-11-25 08:05:27 +0100

    To1 (#4)
    * [TO1] Added entities, gradle properties file and jpa dependency
    
    * [TO1] Added VoteResult class
    
    * [TO1] Added annotations to VoteResult class
    
    * [TO1]Refactored files

--- src/main/java/com/pk/electionappserver/domain/VoteResult.java
+++ src/main/java/com/pk/electionappserver/domain/VoteResult.java
@@ -1,9 +1,35 @@
 package com.pk.electionappserver.domain;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 @Entity(name = "VotesResults")
 public class VoteResult {
-    //TODO: ENCJA DO OMÓWIENIA
-    //@Column(name = "user")
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "user")
+    private User user;
+
+    @Column(name = "election")
+    private Election election;
+
+    @Column(name = "candidate")
+    private Candidate candidate;
+
+    @Column(name = "dataTime")
+    private LocalDateTime localDateTime;
 }
